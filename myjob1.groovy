4282230730a3374583fd43699db334d7a2933c09
Samuel <srohaut@gmail.com>
2019-06-18 11:16:13 +0200

    Update myjob1.groovy

--- myjob1.groovy
+++ myjob1.groovy
@@ -16,3 +16,22 @@ job('My Job 1') {
         shell("npm test")
     }
 }
+
+job('My Job 2') {
+    scm {
+        git('https://github.com/akasam/jenkins-demo1') {  node -> 
+            node / gitConfigName('srohaut  ')
+            node / gitConfigEmail('srohaut@estiam.com')
+        }
+    }
+    triggers {
+        scm('H/10 * * * *')
+    }
+    wrappers {
+        nodejs('NodeJS11')
+    }
+    steps {
+        shell("npm install")
+        shell("npm test")
+    }
+}
