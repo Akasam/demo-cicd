2f7a85f6b977d0aca8b9f8464d06b5626e7d288b
akasam <srohaut@gmail.com>
2024-11-03 21:27:52 +0100

    New code & test

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+const server = app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
+
+module.exports = { app, server };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,14 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
-});
+const request = require('supertest');
+const { app, server } = require('./app');
 
+describe('GET /', () => {
+    afterAll(done => {
+        server.close(done);
+    });
+
+    it('should return wrong message', async () => {
+        const response = await request(app).get('/');
+        expect(response.text).toBe('Wrong message'); // This will fail
+        expect(response.statusCode).toBe(200);
+    });
+});
