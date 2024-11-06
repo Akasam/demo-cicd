94e73aa21e21dc2664223b1226d98ab986cd1bf6
Samuel <srohaut@gmail.com>
2024-11-06 10:41:37 +0100

    Update index.js : traduction en Français

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Bienvenue le Monde !');
 });
 
 const server = app.listen(3000, () => {
